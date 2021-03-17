b6e495037826fad712b6dde90401184e08111a6a
nhidayahj <hidayah.joha@gmail.com>
2021-03-17 23:26:59 +0000

    styling of data in homepage and filter pages

--- travelplan/src/pages/Australia.js
+++ travelplan/src/pages/Australia.js
@@ -2,9 +2,10 @@ import React from 'react'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
+    Input, InputGroup, InputGroupAddon,
     Container,
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button,
+    CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -15,8 +16,7 @@ export default class Australia extends React.Component {
         aus_reviews: [],
         country: [],
         all_users: [],
-        filter_btn: this.props.filter_search,
-
+        filter_btn: "",
     }
 
     async componentDidMount() {
@@ -61,21 +61,6 @@ export default class Australia extends React.Component {
     }
 
 
-
-    // showEachReview(props, id) {
-    //     let showReview = [];
-    //     for (let i of this.state.aus_reviews) {
-    //         if (id === i._id) {
-    //             showReview.push(
-    //                 <div>
-    //                     <p>{props.review_id}</p>
-    //                 </div>
-    //             )
-    //         }
-    //     }
-    //     return showReview;
-    // }
-
     renderAllReview() {
         let aus_accum = [];
         for (let user of this.state.all_users) {
@@ -99,25 +84,6 @@ export default class Australia extends React.Component {
                 }
             }
         }
-        // let aus_accum = [];
-        // for (let i of this.state.aus_reviews) {
-        //     let obj = { review_id: i._id, country_id: i.country };
-
-        //     aus_accum.push(
-        //         <div key={i._id}>
-        //             <Card>
-        //                 <CardImg top width="25%" height="50%" src={i.image_link} alt="Card image cap" />
-        //                 <CardBody>
-        //                     <CardTitle tag="h5">City: {i.city_town}</CardTitle>
-        //                     <CardTitle tag="h5">Category: {i.review_category}</CardTitle>
-        //                     <CardSubtitle tag="h6" className="mb-2 text-muted">Reviewed by: {i.username}</CardSubtitle>
-        //                     <CardText>{i.review_desc}</CardText>
-        //                     <Link to={{ pathname: "/edit", state: obj }}><Button outline color="primary" size="sm">Edit</Button></Link>
-        //                 </CardBody>
-        //             </Card>
-        //         </div>
-        //     )
-        // }
         return aus_accum;
     }
 
@@ -127,61 +93,94 @@ export default class Australia extends React.Component {
         })
     }
 
-
-
+    filterDisplayTitle() {
+        if (this.state.filter_btn === "Accommodation") {
+            return (<h3 className="page-title-display">amazing places to relax in</h3>)
+        } else if (this.state.filter_btn === "Restaurant") {
+            return (<h3 className="page-title-display">spice up your tastebuds</h3>)
+        } else if (this.state.filter_btn === "Activities") {
+            return (<h3 className="page-title-display">travel activities to hype your adventure</h3>)
+        }
+    }
 
     filterDisplay() {
         let filter_accum = [];
+        let heading;
         for (let i of this.state.aus_reviews) {
             if (i.review_category === this.state.filter_btn) {
+                heading = this.filterDisplayTitle()
                 filter_accum.push(
-                    <div key={i._id}>
-                        <p>City: {i.city_town}</p>
-                        <p>Category: {i.review_category}</p>
-                        <p>Review Description: {i.review_desc}</p>
-                        <p>Reviewed by: {i.username}</p>
-                        <Button outline color="primary" size="sm">Update</Button>{' '}
-                        <Button color="danger" size="sm">Delete</Button>
+                    <div key={i._id} className="filter-result">
+                        <div className="filter-img">
+                            <img src={i.image_link} className="filter-img-link"></img>
+                        </div>
+                        <div className="filter-info">
+                            <h3 className="info-title">{i.review_type}</h3>
+                            <div className="info">
+                                <p>Description: {i.review_desc}</p>
+                                <p>Reviewed by: user</p>
+                            </div>
+                            <div className="info-btns">
+                                <div><Button outline color="info" size="sm">edit</Button></div>
+                            </div>
+                        </div>
                     </div>
+
+                    // <div key={i._id}>
+                    //     <p>City: {i.city_town}</p>
+                    //     <p>Category: {i.review_category}</p>
+                    //     <p>Review Description: {i.review_desc}</p>
+                    //     <p>Reviewed by: {i.username}</p>
+                    //     <Button outline color="primary" size="sm">Update</Button>{' '}
+                    //     <Button color="danger" size="sm">Delete</Button>
+                    // </div>
                 )
             }
         }
-        return filter_accum;
+        return { heading, filter_accum };
     }
 
     render() {
         return (
             <React.Fragment>
+
                 {this.renderCountryInfo()}
+                <Container>
+                    <div className="filter-section">
+                        <button value="home" className="filter-btn" onClick={this.filterBtn}>Home</button>
+                        <button value="Accommodation" className="filter-btn" onClick={this.filterBtn}>Accommodation</button>
+                        <button value="Restaurant" className="filter-btn" onClick={this.filterBtn}>Restaurant</button>
+                        <button value="Activities" className="filter-btn" onClick={this.filterBtn}>Activities</button>
+                    </div>
 
-                <div className="filter-section">
-                    <ul>
-                        <li class="active" href="/">Home</li>
-                        <li href="/">Accommodation</li>
-                        <li href="/">Restaurant</li>
-                        <li href="/">Activities</li>
-                    </ul>
-                </div>
+                    <div>
+                        <h3 className="page-title">travellers reviews</h3>
+                    </div>
+                    <div className="page-search">
+                        <InputGroup>
+                            <Input placeholder="search by city, keywords, username .." />
+                            <InputGroupAddon addonType="append">
+                                <Button outline color="info">Search</Button>
+                            </InputGroupAddon>
+                        </InputGroup>
+                    </div>
+
+                    <div style={{ display: this.state.filter_btn === "home" || this.state.filter_btn === "" ? 'block' : 'none' }}>
+                        {this.renderAllReview()}
+                    </div>
+                    <div style={{
+                        display: this.state.filter_btn === "Accommodation" || this.state.filter_btn === "Restaurant"
+                            || this.state.filter_btn === "Activities"
+                            ? 'block' : 'none'
+                    }}>
+                        {this.filterDisplay().heading}
+                        {this.filterDisplay().filter_accum}
+
+                    </div>
+
+                </Container>
 
 
-                <div>
-                    <button name="filter_btn" value="home" onClick={this.filterBtn}>Home</button>
-                    <button name="filter_btn" value="Accommodation" onClick={this.filterBtn}>Accommodation</button>
-                    <button name="filter_btn" value="Restaurant" onClick={this.filterBtn}>Restaurant</button>
-                    <button name="filter_btn" value="Activities" onClick={this.filterBtn}>Travel Activities</button>
-                </div>
-                <div style={{ display: this.state.filter_btn === "home" || this.state.filter_btn === undefined ? 'block' : 'none' }}>
-                    <h3>Reviews</h3>
-                    {this.renderAllReview()}
-                </div>
-                <div style={{
-                    display: this.state.filter_btn === "Accommodation" || this.state.filter_btn === "Restaurant"
-                        || this.state.filter_btn === "Activities"
-                        ? 'block' : 'none'
-                }}>
-                    {this.filterDisplay()}
-
-                </div>
             </React.Fragment>
         )
     }
