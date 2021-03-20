41c8a5088203baa0632a4aef8a39d87e4ce37c66
nhidayahj <hidayah.joha@gmail.com>
2021-03-20 11:41:21 +0000

    looping all the details in a review

--- travelplan/src/pages/Australia.js
+++ travelplan/src/pages/Australia.js
@@ -5,7 +5,7 @@ import {
     Input, InputGroup,
     Container, Row, Col,
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
+    CardTitle, CardSubtitle, Button, Badge
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -21,7 +21,6 @@ export default class Australia extends React.Component {
         aus_users: [],
         queryCity: "",
         queryTags: [],
-        queryReviewer: "",
         filter_btn: "",
         newResult: ""
     }
@@ -77,34 +76,51 @@ export default class Australia extends React.Component {
                 </div>
             </div>
         )
+    }
 
+    userReview(userId) {
+        for (let user of this.state.aus_users) {
+            if (user._id === userId) {
+                return user.username
+            }
+        }
+    }
+
+    reviewTags(tags) {
+        let tag_pill = [];
+        for (let tag of tags) {
+            tag_pill.push(
+                <Badge pill className="pill-tags">{tag}</Badge>
+            )
+        }
+        return tag_pill;
+    }
+
+    reviewType(review_type){
+       return (<Badge pill className="pill-type">{review_type}</Badge>)
     }
 
 
     renderAllReview() {
         let aus_accum = [];
-        for (let user of this.state.aus_users) {
-            for (let user_review of this.state.aus_reviews) {
-                if (user._id === user_review.user) {
-                    let obj = { review_id: user_review._id, country_id: user_review.country };
-                    aus_accum.push(
-                        <div key={user_review._id}>
-                            <Card className="card-all-reviews">
-                                <CardImg top width="25%" height="50%" src={user_review.image_link} alt="Card image cap" />
-                                <CardBody>
-                                    <CardTitle tag="h5">City: {user_review.city_town}</CardTitle>
-                                    <CardTitle tag="h5">Category: {user_review.review_category}</CardTitle>
-                                    <CardSubtitle tag="h6" className="mb-2 text-muted">Reviewed by: {user.username}</CardSubtitle>
-                                    <CardText>{user_review.review_desc}</CardText>
-                                    <Link to={{ pathname: "/edit", state: obj }}><Button outline color="primary" size="sm">Edit</Button></Link>
-                                </CardBody>
-                            </Card>
-                        </div>
 
-                    )
-                }
-            }
+        for (let user_review of this.state.aus_reviews) {
+            let obj = { review_id: user_review._id, country_id: user_review.country };
+            aus_accum.push(
+                <div key={user_review._id}>
+                    <Card className="card-all-reviews">
+                        <CardImg top width="25%" height="50%" src={user_review.image_link} alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle tag="h5">City: {user_review.city_town}</CardTitle>
+                            <CardTitle tag="h5">Category: {user_review.review_category}</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Reviewed by: {this.userReview(user_review.user)}</CardSubtitle>
+                            <CardText>{user_review.review_desc}</CardText>
+                            <Link to={{ pathname: "/edit", state: obj }}><Button outline color="primary" size="sm">Edit</Button></Link>
+                        </CardBody>
+                    </Card>
+                </div>)
         }
+
         return aus_accum;
     }
 
@@ -129,6 +145,7 @@ export default class Australia extends React.Component {
         let heading;
         for (let i of this.state.aus_reviews) {
             if (i.review_category === this.state.filter_btn) {
+                let obj = { review_id: i._id, country_id: i.country };
                 heading = this.filterDisplayTitle()
                 filter_accum.push(
                     <div key={i._id} className="filter-result">
@@ -136,13 +153,16 @@ export default class Australia extends React.Component {
                             <img src={i.image_link} className="filter-img-link" alt="uploaded img"></img>
                         </div>
                         <div className="filter-info">
-                            <h3 className="info-title">{i.review_type}</h3>
+                            <h4 className="info-title" style={{fontWeight:"bold"}}>Australia, {i.city_town}</h4>
+                            <h6 className="info-title" style={{fontWeight:"bold"}}>Review Type: {this.reviewType(i.review_type)}</h6>
                             <div className="info">
+                            <p style={{fontWeight:"bold"}}>tags: <span>{this.reviewTags(i.review_tags)}</span></p> 
                                 <p>Description: {i.review_desc}</p>
-                                <p>Reviewed by: user</p>
+                                <p>Reviewed by: {this.userReview(i.user)}</p>
                             </div>
                             <div className="info-btns">
-                                <div><Button outline color="info" size="sm">edit</Button></div>
+                                <div><Link to={{ pathname: "/edit", state: obj }}>
+                                    <Button outline color="primary" size="sm" className="info-edit-btns">Edit</Button></Link></div>
                             </div>
                         </div>
                     </div>
@@ -161,42 +181,56 @@ export default class Australia extends React.Component {
 
     userSearch = async () => {
         let searchQuery;
-        if (this.state.queryCity && this.state.queryTags) {
+        if (this.state.queryCity && this.state.queryTags !== "") {
             searchQuery = await axios.post(baseUrl, {
-                queryCity:this.state.queryCity,
-                queryTags:this.state.queryTags
-            })
-            this.setState({
-                newResult:searchQuery.data
-            })
-            console.log("Search by City & Tags: ",searchQuery.data)
-        } else if (this.state.queryCity){
-            searchQuery = await axios.post(baseUrl, {
-                queryCity:this.state.queryCity
+                queryCity: this.state.queryCity,
+                queryTags: this.state.queryTags
             })
             this.setState({
                 newResult: searchQuery.data
             })
-            console.log("Search by city: " ,searchQuery.data)
+            console.log("Search by City & Tags: ", searchQuery.data)
+        } else if (this.state.queryCity) {
+            searchQuery = await axios.post(baseUrl, {
+                queryCity: this.state.queryCity
+            })
+            this.setState({
+                newResult: searchQuery.data
+            })
+            console.log("Search by city: ", searchQuery.data)
         } else if (this.state.queryTags) {
             searchQuery = await axios.post(baseUrl, {
-                queryTags:this.state.queryTags
+                queryTags: this.state.queryTags
             })
             this.setState({
                 newResult: searchQuery.data
             })
-            console.log("Search by tags: " ,searchQuery.data)
+            console.log("Search by tags: ", searchQuery.data)
         }
-        // let queryBox = { queryCity: this.state.queryCity}
-        // console.log(queryBox)
-        // let searchQuery = await axios.post(baseUrl, {
-        //     queryCity:this.state.queryCity,
-        //     queryTags:[this.state.queryTags]
-        // })
-        // console.log(searchQuery.data);
-        // this.setState({
-        //     newResult: searchQuery.data
-        // })
+    }
+
+    renderUserSearch = () => {
+        let search_accum = [];
+        for (let i of this.state.newResult) {
+            search_accum.push(
+                <div key={i._id} className="filter-result">
+                    <div className="filter-img">
+                        <img src={i.image_link} className="filter-img-link" alt="uploaded img"></img>
+                    </div>
+                    <div className="filter-info">
+                        <h3 className="info-title">City: {i.city_town}</h3>
+                        <div className="info">
+                            <p>Description: {i.review_desc}</p>
+                            <p>Reviewed by: user</p>
+                        </div>
+                        <div className="info-btns">
+                            <div><Button outline color="info" size="sm">edit</Button></div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        return search_accum;
     }
 
     render() {
@@ -243,9 +277,12 @@ export default class Australia extends React.Component {
                         </div>
                     </div>
 
-                    <div style={{ display: this.state.filter_btn === "Home" || this.state.filter_btn === "" ? 'block' : 'none' }}
+                    <div style={{
+                        display: (this.state.filter_btn === "Home" || this.state.filter_btn === "")
+                            && this.state.newResult === "" ? 'block' : 'none'
+                    }}
                         className="all-reviews">
-
+                        <h3 className="page-title-display">Our Reviews</h3>
                         {this.renderAllReview()}
                     </div>
                     <div style={{
@@ -256,6 +293,10 @@ export default class Australia extends React.Component {
                         {this.filterDisplay().heading}
                         {this.filterDisplay().filter_accum}
 
+                    </div>
+                    <div style={{ display: this.state.newResult !== "" ? "block" : "none" }}>
+                        <h3 className="page-title-display">Filtered Search</h3>
+                        {this.renderUserSearch()}
                     </div>
 
                 </Container>
